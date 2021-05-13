b59be7f3eaf3ec4938d7f72a303f4ffffb098b02
Rashid Lazytech <lazyt3ch@gmail.com>
2021-05-13 09:56:55 +0500

    change messages in ui/Home.js

--- src/ui/Home.js
+++ src/ui/Home.js
@@ -6,8 +6,20 @@ export const languages = [
 export const uiStrings = {
     'welcome':
     {
-        en: "Welcome to our website!",
-        ru: "Добро пожаловать на наш сайт!",
+        // en: "Welcome to our website!",
+        // ru: "Добро пожаловать на наш сайт!",
+        en: "Welcome!",
+        ru: "Добро пожаловать!",
+    },
+    'offer_to_play':
+    {
+        en: "Would you like to play a simple game?",
+        ru: "Не желаете ли сыграть в простую игру?",
+    },
+    'offer_to_select_game':
+    {
+        en: "Select a game using the drop-down list (see the top-left corner).",
+        ru: "Выберите игру в выпадающем списке (в левом верхнем углу экрана).",
     },
     
 
