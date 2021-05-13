05b4e74425333693b7c620ae84870d2e204414f9
Rashid Lazytech <lazyt3ch@gmail.com>
2021-05-13 09:38:26 +0500

    clean up WhoPlaysFirst

--- src/components/WhoPlaysFirst.svelte
+++ src/components/WhoPlaysFirst.svelte
@@ -5,21 +5,17 @@
     import { globalLanguage } from '../stores.js';
     
     export let whoBegins;    
-    // let whoBegins;
 
     let language;
 	const unsubscribe = globalLanguage.subscribe(value => {
         language = value;        
-		// console.log("whoBegins:  language =", language);
     });     
-
 
     let highlighted = false;
 
     const dispatch = createEventDispatcher();
 
     function handleWhoBegins() {
-        // console.log("FUNCTION: handleWhoBegins;  whoBegins =", whoBegins);
         dispatch('whoBegins', whoBegins);
     }
 </script>
@@ -28,7 +24,6 @@
 <style>
     .who-plays-first {
         border-width: 1px;
-        /* border-color: gray; */
         border-color: transparent;
         margin-left: 0;
         padding: 0;
@@ -54,15 +49,10 @@
         -o-user-select: none;
         user-select: none;
     }        
-
-    /* .left-margin {
-        margin-left: 1rem;
-    } */
 </style>
 
 
 <!-- WHO PLAYS FIRST radio buttons etc -->
-<!-- on:change={() => oppoTurn = true}  -->
 <fieldset id='who-plays-first' transition:fade="{{delay: 100, duration: 500}}"
     class="who-plays-first limited-width margin-after {highlighted? 'highlighted': ''}"
     on:change={handleWhoBegins}    
