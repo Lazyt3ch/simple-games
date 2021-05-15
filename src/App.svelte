bedb08dc5414dfa318340c0881660d89272deb8e
Rashid Lazytech <lazyt3ch@gmail.com>
2021-05-15 12:21:48 +0500

    improve page selector in App; clean up App

--- src/App.svelte
+++ src/App.svelte
@@ -3,48 +3,31 @@
 	import {push, pop, replace} from 'svelte-spa-router'	
 
 	import { languages, uiStrings as ui } from './ui/App.js';
-	// import { globalLanguageIndex } from './stores.js';
+	import { uiStrings as homeUi } from './ui/Home.js';
 
 	// currentGame is a bugfix that disables transitions when routing
 	import { globalLanguage, currentGame } from './stores.js';
-	// console.log("globalLanguageIndex =", globalLanguageIndex);
-
-	// import { currentGame } from './stores.js';
-
 
 	import Home from './routes/Home.svelte';
 
 	import TicTacToe from './routes/TicTacToe.svelte';
 	import Battleship from './routes/Battleship.svelte';
-	// let gamelinks = null;
 
 	import { gameId as gameId1, gameName as gameName1 } from './ui/TicTacToe.js';
 	import { gameId as gameId2, gameName as gameName2 } from './ui/Battleship.js';
 
-	/*
-	let globLang = null;
+
+  let language;
 
 	const unsubscribe = globalLanguage.subscribe(value => {
-		globLang = value;
-		console.log("globLang =", globLang);
-	});
-	*/
-
-    let language;
-    
-    // console.log("App:  $globalLanguage =", $globalLanguage);
-
-	const unsubscribe = globalLanguage.subscribe(value => {
-        language = value;        
-		// console.log("App:  language =", language);
+		language = value;        
 	});    
 
 
 	let curGame;
 
 	const unsubscribe2 = currentGame.subscribe(value => {
-        curGame = value;        
-		// console.log("App:  curGame =", curGame);
+		curGame = value;        
 	});    
 
 	const routes = { // Exact paths
@@ -55,26 +38,14 @@
 		'*': Home,
 	}
 
-	// let language = "en";
-	// let language = languages[0].short;
-	// export language;
-	// let selectedLang = languages[0];
 	let selectedLang;
-	// let language = languages[langIndex].short;
-	// let language = globLang;
-	// console.log("language =", language);
-	// const globalLanguage = writable(language);
 	
 	function updateGlobalLanguage() {
-		// console.log("App:  trying to update globalLanguage...");
 		globalLanguage.update(() => language);
-		// console.log("App:  $globalLanguage =", $globalLanguage);
 	}
 
 	function updateCurrentGame() {
-		// console.log("App:  trying to update currentGame...");
 		currentGame.update(() => selectedGameId);
-		// console.log("App:  $currentGame =", $currentGame);
 	}	
 
 	$: {
@@ -85,33 +56,41 @@
 		if (selectedGameId !== null) updateCurrentGame();
 	}
 
-	let gameIndex = null;
-	let game = null;
-	let games;
-	updateGameNames();
-	let selectedGameId = games[0].id;
+	// let gameIndex = null;
+	// let game = null;
 
+	let games = [
+		{ id: 'home', name: ui['select_game'] },
+		{ id: gameId1, name: gameName1 },
+		{ id: gameId2, name: gameName2 },	
+	];
+
+	let selectedGameId = games[0].id;
+	updateGameNames();
+	
 	function updateGameNames() {
 		games = [
-			{ id: 'home', name: ui['select_game'] },
+			{ id: 'home', 
+				name: (
+					selectedGameId === 'home' 
+						? ui['select_game'] 
+						: homeUi['home_page']
+				) 
+			},
 			{ id: gameId1, name: gameName1 },
 			{ id: gameId2, name: gameName2 },
 		]			
 	}	
 
 	$: {
-		if (language !== null) updateGameNames(); 
+		if (language !== null || selectedGameId !== null) updateGameNames(); 
 	}
 
 	$: {
 		if (selectedGameId !== null) {
-			// console.log("selectedGameId =", selectedGameId);
-			// console.log(`/${selectedGameId}`);
 			push(`/${selectedGameId}`);
 		}
 	}
-
-	// console.log("games =", games);
 
 	$: language = language;
 
@@ -128,27 +107,15 @@
 		font-size: 1rem;
 	}
 
-    .rightish {
-        /* margin-left: 70%;
-        margin-right: auto; */
+	.rightish {
 		float: right;
-        text-align: left;
-    }	
+		text-align: left;
+	}	
 
 	.leftish {
 		float: left;
 		text-align: left;
 	}
-
-
-	/*
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	*/
 
 	main {
 		width: 100%;
@@ -156,36 +123,20 @@
 		margin: 0 auto;
 	}
 	
-	/*
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	*/
-
-	/* @media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	} */
 </style>
 
 
 <main>
-
 	<!-- Game selector -->
-	<!--			bind:value={selectedGame} on:change={() => game = selectedGame} -->
-	<!-- 			bind:value={selectedGameId} on:change={() => selectedGameId=games[0].id} -->
 	<label for="game-select" >
 		&nbsp;
 		<select name="game-name" id="game-select" class="game-select leftish"
 			bind:value={selectedGameId} 
 		>
 			{#each games as game, index}
-				<option value="{games[index].id}" disabled={index === 0}> { game.name[language] } </option> 
-				<!-- <option value="{game}" > { game } </option> -->
+				<option value="{games[index].id}" disabled={selectedGameId === games[index].id}> 
+					{ game.name[language] } 				
+				</option> 
 			{/each}
 		</select>
 	</label>
